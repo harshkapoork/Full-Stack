f6a4febc275b36502cca8ccb7b11b5815fc2c930
harshkapoork <85664811+harshkapoork@users.noreply.github.com>
2024-07-30 18:39:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 const cors = require("cors")
 const dotenv = require("dotenv")
+//new
 
 dotenv.config();
 // middleware
@@ -12,7 +13,7 @@ app.use(express.json())
 
 // database connection
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true })
+mongoose.connect(process.env.DB_STRING, )
     .then(() => {
     console.log("Db connection Started")
 })
